9a2b7efad0a65769df2c51b87c16603632d9d5cd
Ben Greanya <bengreanya@gmail.com>
2022-11-08 11:14:03 -0800

    created render js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Imports */
-
+import { renderGoblin } from './render-utils.js';
 /* Get DOM Elements */
 const goblinsListEl = document.querySelector('goblins');
 const formEl = document.querySelector('form');
@@ -7,7 +7,13 @@ const playerHPEl = document.querySelector('#player-hp');
 const goblinKillsEl = document.querySelector('#goblin-kills');
 const playerImg = document.querySelector('#player-img');
 /* State */
-
+const goblins = [
+    { id: 1, name: 'Mean One', hp: 5 },
+    { id: 2, name: 'Big Bad One', hp: 5 },
+];
+let currentId = 3;
+let playerHP = 5;
+let killedGoblinCount = 0;
 /* Events */
 
 /* Display Functions */
