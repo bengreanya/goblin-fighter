5e1868d88d8309a691ca4778174cf1db73fe7bad
Ben Greanya <bengreanya@gmail.com>
2022-11-08 10:17:27 -0800

    added player image and DOM elements

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const goblinsListEl = document.querySelector('goblins');
+const formEl = document.querySelector('form');
+const playerHPEl = document.querySelector('#player-hp');
+const goblinKillsEl = document.querySelector('#goblin-kills');
+const playerImg = document.querySelector('#player-img');
 /* State */
 
 /* Events */
